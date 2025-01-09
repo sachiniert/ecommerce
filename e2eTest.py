917caa2a1136e39fe0acd1af017aa4cf2d8dd1ca
Sachin Yadav <sachin.yadav.sdet@mail.com>
2025-01-09 18:48:48 +0530

    This is comment

--- e2eTest.py
+++ e2eTest.py
@@ -17,7 +17,6 @@ for product in products:
     productName = product.find_element(By.XPATH,"div/h4/a").text
     if productName == "Blackberry":
         product.find_element(By.XPATH,"div/button").click()
-
 driver.find_element(By.CSS_SELECTOR,"a[class*='btn-primary']").click()
 driver.find_element(By.XPATH,"//button[@class='btn btn-success']").click()
 driver.find_element(By.ID,"country").send_keys("ind")
@@ -29,4 +28,4 @@ driver.find_element(By.CSS_SELECTOR,"[type='submit']").click()
 successText=driver.find_element(By.CLASS_NAME,"alert-success").text
 assert  "Success! Thank you!" in successText
 driver.close()
-
+#hh
